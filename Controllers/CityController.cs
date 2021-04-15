7f684a02524a43ba3902275b442495501700b268
anxhelinazhuri <anxhelinazhuri@gmail.com>
2021-04-15 17:23:22 +0200

    First commit

---
+++ Controllers/CityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+                
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Getstrings()
+        {
+            return new string[] { "Atalanta", "New york", "Dettroit","Masachusets"};
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return  "Atalanta";
+        }
+
+
+    }
+}
